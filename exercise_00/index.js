d4d36ad451ce3a4d28479abf9bd59abd98961c8c
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2024-11-30 13:22:41 +0100

    fix: solve a console.log bug in exercise_00

--- exercise_00/index.js
+++ exercise_00/index.js
@@ -1,10 +1,10 @@
 //import and execute
 import raiseValueToPower2 from "./raiseValueToPower2.js";
 
-console("Ex 00");
+console.log("Ex 00");
 console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
-console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
-console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
-console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
-console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("raiseValueToPower2", raiseValueToPower2(3)); // Expected: 9
+console.log("raiseValueToPower2", raiseValueToPower2(7)); // Expected: 49
+console.log("raiseValueToPower2", raiseValueToPower2(9)); // Expected: 81
+console.log("raiseValueToPower2", raiseValueToPower2(12)); // Expected: 144
 console.log("-----------------------------------------");
