72fbe12a85dce50301f5a3da4b22e7eaf94a60ea
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2024-11-30 13:20:11 +0100

    feat: add more tests to exercise_00

--- exercise_00/index.js
+++ exercise_00/index.js
@@ -1,4 +1,10 @@
 //import and execute
 import raiseValueToPower2 from "./raiseValueToPower2.js";
 
+console("Ex 00");
 console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("raiseValueToPower2", raiseValueToPower2(2)); // Expected: 4
+console.log("-----------------------------------------");
